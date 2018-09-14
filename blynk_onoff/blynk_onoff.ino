7a3bb1090627cd51401440e0d4b2ce2be8ee47a5
adeams <lookmun@gmail.com>
2018-09-14 15:24:33 +0700

    add fund button Hardware sync button virtual in app

--- blynk_onoff/blynk_onoff.ino
+++ blynk_onoff/blynk_onoff.ino
@@ -12,32 +12,39 @@
 // define button set token
 #define SW_PIN 0
 
-const int btnPin0 = D0;
-const int btnPin1 = D1;
-const int btnPin2 = D2;
-const int btnPin3 = D4;
-const int btnPin4 = D5;
-const int btnPin5 = D6;
-const int btnPin6 = D7;
-const int btnPin7 = D8;
-const int btnPin8 = D9;
-const int btnPin = D10;
+//button Hardware
+//const int btnPin0 = D0;
+const int btnPin1 = D6;
+const int btnPin2 = D7;
+const int btnPin3 = D5;
+const int btnPin4 = D9;
 
-int ledState = LOW;
-int btnState = HIGH;
+//button Virtual
+const int AppBtn1 = D1;
+const int AppBtn2 = D2;
+const int AppBtn3 = D4;
+const int AppBtn4 = D8;
+
+//status output hardware
+bool ledState0 = false;
+bool ledState1 = false;
+bool ledState2 = false;
+bool ledState3 = false;
+bool ledState4 = false;
+
+//status botton config
+int ledState = false;
 int btnStateConfig = HIGH;
 
-boolean btnState0 = false;
-boolean btnState1 = false;
-boolean btnState2 = false;
-boolean btnState3 = false;
-boolean btnState4 = false;
-boolean btnState5 = false;
-boolean btnState6 = false;
-boolean btnState7 = false;
-boolean btnState8 = false;
-boolean btnState9 = false;
-boolean btnState10 = false;
+//satatus 
+int btnState0 = LOW;
+int btnState1 = LOW;
+int btnState2 = LOW;
+int btnState3 = LOW;
+int btnState4 = LOW;
+int btnState5 = LOW;
+
+
 
 char mqtt_server[40];
 char mqtt_port[6] = "";
@@ -50,7 +57,7 @@ int addr = 0;
 File f;
 Ticker ticker;
 BlynkTimer timer;
-void checkPhysicalButton();
+//void checkPhysicalButton();
 const char *filename = "/config.json";  // <- SD library uses 8.3 filenames
 
 // Every time we connect to the cloud...
@@ -61,29 +68,15 @@ BLYNK_CONNECTED() {
   Blynk.syncVirtual(V2);
   Blynk.syncVirtual(V3);
   Blynk.syncVirtual(V4);
-  Blynk.syncVirtual(V5);
-  Blynk.syncVirtual(V6);
-  Blynk.syncVirtual(V7);
-  Blynk.syncVirtual(V8);
-
   // Alternatively, you could override server state using:
   //Blynk.virtualWrite(V2, ledState);
   Serial.println("BLYNK_CONNECTED");
 }
 
 // When App button is pushed - switch the state
-BLYNK_WRITE(V0) {
-  ledState = param.asInt();
-  digitalWrite(btnPin0, ledState);
-  EEPROM.write(addr+0, ledState);
-  EEPROM.commit();
-  Serial.println("BLYNK_WRITEV0");
-}
-
-// When App button is pushed - switch the state
 BLYNK_WRITE(V1) {
   ledState = param.asInt();
-  digitalWrite(btnPin1, ledState);
+  digitalWrite(AppBtn1, ledState);
   EEPROM.write(addr+1, ledState);
   EEPROM.commit();
   Serial.println("BLYNK_WRITEV1");
@@ -92,7 +85,7 @@ BLYNK_WRITE(V1) {
 // When App button is pushed - switch the state
 BLYNK_WRITE(V2) {
   ledState = param.asInt();
-  digitalWrite(btnPin2, ledState);
+  digitalWrite(AppBtn2, ledState);
   EEPROM.write(addr+2, ledState);
   EEPROM.commit();
   Serial.println("BLYNK_WRITEV2");
@@ -101,7 +94,7 @@ BLYNK_WRITE(V2) {
 // When App button is pushed - switch the state
 BLYNK_WRITE(V3) {
   ledState = param.asInt();
-  digitalWrite(btnPin3, ledState);
+  digitalWrite(AppBtn3, ledState);
   EEPROM.write(addr+3, ledState);
   EEPROM.commit();
   Serial.println("BLYNK_WRITEV3");
@@ -110,66 +103,47 @@ BLYNK_WRITE(V3) {
 // When App button is pushed - switch the state
 BLYNK_WRITE(V4) {
   ledState = param.asInt();
-  digitalWrite(btnPin4, ledState);
+  digitalWrite(AppBtn4, ledState);
   EEPROM.write(addr+4, ledState);
   EEPROM.commit();
   Serial.println("BLYNK_WRITEV4");
 }
-
-// When App button is pushed - switch the state
-BLYNK_WRITE(V5) {
-  ledState = param.asInt();
-  digitalWrite(btnPin5, ledState);
-  EEPROM.write(addr+5, ledState);
-  EEPROM.commit();
-  Serial.println("BLYNK_WRITEV5");
+void scankey(){
+  checkPhysicalButton(&btnPin1,&btnState1,&ledState1,1);
+  checkPhysicalButton(&btnPin2,&btnState2,&ledState2,2);
+  checkPhysicalButton(&btnPin3,&btnState3,&ledState3,3);
+  checkPhysicalButton(&btnPin4,&btnState4,&ledState4,4);
 }
-
-// When App button is pushed - switch the state
-BLYNK_WRITE(V6) {
-  ledState = param.asInt();
-  digitalWrite(btnPin6, ledState);
-  EEPROM.write(addr+6, ledState);
-  EEPROM.commit();
-  Serial.println("BLYNK_WRITEV6");
-}
-
-// When App button is pushed - switch the state
-BLYNK_WRITE(V7) {
-  ledState = param.asInt();
-  digitalWrite(btnPin7, ledState);
-  EEPROM.write(addr+7, ledState);
-  EEPROM.commit();
-  Serial.println("BLYNK_WRITEV7");
-}
-
-// When App button is pushed - switch the state
-BLYNK_WRITE(V8) {
-  ledState = param.asInt();
-  digitalWrite(btnPin8, ledState);
-  EEPROM.write(addr+8, ledState);
-  EEPROM.commit();
-  Serial.println("BLYNK_WRITEV8");
-}
-
-void checkPhysicalButton(){
-  if (digitalRead(btnPin) == LOW) {
+void checkPhysicalButton(const int *btnPin,int *btnState,bool *ledState,int vo){
+  if (digitalRead(*btnPin) == LOW) {
     // btnState is used to avoid sequential toggles
-    if (btnState != LOW) {
+    if (*btnState != LOW) {
 
       // Toggle LED state
-      ledState = !ledState;
-      digitalWrite(btnPin0, ledState);
-      digitalWrite(btnPin3, !ledState);
+      *ledState = !(*ledState);
+      digitalWrite(*btnPin, *ledState);
       // Update Button Widget
-      Blynk.virtualWrite(V0, ledState);
-      Blynk.virtualWrite(V3, !ledState);
+      Serial.printf("press button VO%d\n\r",vo);
+      switch(vo){
+        case 1:
+           Blynk.virtualWrite(V1, *ledState);
+        break;
+        case 2:
+           Blynk.virtualWrite(V2, *ledState);
+        break;
+        case 3:
+           Blynk.virtualWrite(V3, *ledState);
+        break;
+        case 4:
+           Blynk.virtualWrite(V4, *ledState);
+        break;
+        }
     }
-    btnState = LOW;
-    Serial.println("LOW");
+    *btnState = LOW;
+    //Serial.println(vo);
   } else {
     //Serial.println("HIGH");
-    btnState = HIGH;
+    *btnState = HIGH;
   }
 }
 
@@ -203,10 +177,13 @@ void tick(){
 
 void tick1(){
   //toggle state
-  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
-  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
-  delay(10);
-  digitalWrite(BUILTIN_LED, state);     // set pin to the opposite state
+  if(++count > 5){
+    count = 0;
+    int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
+    digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+    delay(10);
+    digitalWrite(BUILTIN_LED, state);     // set pin to the opposite state
+  }
 }
 
 //void buttonLedWidget(const int *btnPin,boolean *btnState,int Vo){
@@ -557,20 +534,15 @@ bool setWifiManager(){
 
 
 void io_refress(){
-    digitalWrite(btnPin0,EEPROM.read(addr+0));
-    digitalWrite(btnPin1,EEPROM.read(addr+1));
-    digitalWrite(btnPin2,EEPROM.read(addr+2));
-    digitalWrite(btnPin3,EEPROM.read(addr+3));
-    digitalWrite(btnPin4,EEPROM.read(addr+4));
-    digitalWrite(btnPin5,EEPROM.read(addr+5));
-    digitalWrite(btnPin6,EEPROM.read(addr+6));
-    digitalWrite(btnPin7,EEPROM.read(addr+7));
-    digitalWrite(btnPin8,EEPROM.read(addr+8));
+    digitalWrite(AppBtn1,EEPROM.read(addr+1));
+    digitalWrite(AppBtn2,EEPROM.read(addr+2));
+    digitalWrite(AppBtn3,EEPROM.read(addr+3));
+    digitalWrite(AppBtn4,EEPROM.read(addr+4));
     //digitalWrite(D10,EEPROM.read(addr+10));
-    Serial.printf("EEPROM.read(addr+0)>%d\n\r",EEPROM.read(addr+0));
-    Serial.printf("EEPROM.read(addr+1)>%d\n\r",EEPROM.read(addr+1));
-    Serial.printf("EEPROM.read(addr+2)>%d\n\r",EEPROM.read(addr+2));
-    Serial.printf("EEPROM.read(addr+4)>%d\n\r",EEPROM.read(addr+4));  
+//    Serial.printf("EEPROM.read(addr+0)>%d\n\r",EEPROM.read(addr+0));
+//    Serial.printf("EEPROM.read(addr+1)>%d\n\r",EEPROM.read(addr+1));
+//    Serial.printf("EEPROM.read(addr+2)>%d\n\r",EEPROM.read(addr+2));
+//    Serial.printf("EEPROM.read(addr+4)>%d\n\r",EEPROM.read(addr+4));  
 }
 
 void setup() {
@@ -579,16 +551,15 @@ void setup() {
     while (!Serial) continue;
     Serial.println("");
     pinMode(SW_PIN, INPUT_PULLUP); 
-    pinMode(btnPin, INPUT_PULLUP); 
-    pinMode(btnPin0, OUTPUT);
-    pinMode(btnPin1, OUTPUT);
-    pinMode(btnPin2, OUTPUT);
-    pinMode(btnPin3, OUTPUT);
-    pinMode(btnPin4, OUTPUT);
-    pinMode(btnPin5, OUTPUT);
-    pinMode(btnPin6, OUTPUT);
-    pinMode(btnPin7, OUTPUT);
-    pinMode(btnPin8, OUTPUT);
+    pinMode(btnPin1, INPUT_PULLUP);
+    pinMode(btnPin2, INPUT_PULLUP);
+    pinMode(btnPin3, INPUT_PULLUP);
+    pinMode(btnPin4, INPUT_PULLUP);
+    pinMode(AppBtn1, OUTPUT);
+    pinMode(AppBtn2, OUTPUT);
+    pinMode(AppBtn3, OUTPUT);
+    pinMode(AppBtn4, OUTPUT);
+    pinMode(BUILTIN_LED, OUTPUT);
     EEPROM.begin(512);
     io_refress();
     // always use this to "mount" the filesystem
@@ -599,37 +570,14 @@ void setup() {
     io_refress();
   
     // Setup a function to be called every 100 ms
-    timer.setInterval(100L, checkPhysicalButton);
+    //timer.setInterval(100L, checkPhysicalButton);
     timer.setInterval(1000L, checkButtonIsConfig);
+    timer.setInterval(1000L, tick1);
+    timer.setInterval(100L, scankey);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
     Blynk.run();
     timer.run();
-
-    if(++count >= 100000){
-      count = 0;
-      tick1();
-      //buttonLedWidget(&btnPin3,&btnState3,3);
-      //buttonLedWidget(int btnPin,boolean btnState,int Vo){
-    }
-
-    
-//  //reset saved settings
-//  if(digitalRead(SW_PIN) == LOW){ // Press button
-//    delay(1000); 
-//    int i = 5;
-//    Serial.printf("Reset wifi config?:\n\r");
-//    while(digitalRead(SW_PIN) == LOW){
-//      Serial.print(String(i)+" "); 
-//      delay(1000);
-//      if((--i) == 0){
-//        setWifiManager(); 
-//        ticker.detach(); 
-//      }
-//    }
-//    Serial.printf("\n\rexit wifi config\n\r");
-//  } 
-    
 }
